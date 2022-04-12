e4c305913121b9eb6ec38839e19ba278c360f8ed
seergeist <kusamaxi@gmail.com>
2022-04-12 10:05:45 -0400

    remove nodemodules

--- build/server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const entry = 'pages/__layout.svelte-514149bd.js';
-export const js = ["pages/__layout.svelte-514149bd.js","chunks/index-60624bc8.js"];
-export const css = ["assets/pages/__layout.svelte-b5dec940.css"];
