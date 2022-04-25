564958e419917b30dd1c8d6d577564251acdb190
seer geist <kusamaxi@gmail.com>
2022-04-25 14:57:34 -0500

    fix card text sizes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -56,17 +56,17 @@
 </svelte:head>
 
 <section class="mx-auto">
-	<div class="flex flex-col p-1 md:p-2">
+	<div class="flex flex-col">
 		<div
-			class="bg-gradient-to-bl to-#0100FE/90 from-#C000B0/95 w-full h-100 md:h-118 rounded-3xl mt-4 mb-8 p-8 md:p-12 relative"
+			class="bg-gradient-to-bl to-#0100FE/90 from-#C000B0/95 w-full h-100 mb-8 md:h-118 rounded-3xl p-8 md:p-12 relative"
 		>
 			<div class="absolute right-0 z-1">
 				<img class="w-55/100 mt-10 sm:mt-0 md:w-2/3 float-right sm:mr-5 lg:mr-10 opacity-85 md:opacity-95" src={hero.img} alt={hero.name} />
 			</div>
 			<div class="absolute z-2 w-60/100">
-				<h2 class="font-grotesk font-bold text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4">{hero.name}</h2>
+				<h2 class="font-grotesk font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4">{hero.name}</h2>
 			</div>
-			<p class="font-lato absolute w-1/2 text-lg md:text-xl bottom-32 md:bottom-40 z-2">
+			<p class="font-lato absolute w-1/2 text-md md:text-lg md:text-xl bottom-32 md:bottom-40 z-2">
 				{hero.desc}
 			</p>
 			<button class="font-grotesk bg-#6236FF/80 hover:bg-#6236FF hover:animate-swing pr-6 pl-4 md:pl-6 md:pr-10 py-3 md:py-4 md:my-5 rounded-3xl absolute bottom-8 md:bottom-12 z-2">
@@ -80,9 +80,9 @@
 					<img class="w-55/100 mt-10 sm:mt-0 md:w-2/3 float-right sm:mr-5 lg:mr-10 opacity-85 md:opacity-95" src={page.img} alt={page.name} />
 				</div>
 				<div class="absolute z-2 w-60/100">
-					<h2 class="font-grotesk font-bold text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4">{page.name}</h2>
+					<h2 class="font-grotesk font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4">{page.name}</h2>
 				</div>
-				<p class="font-lato absolute w-35/100 md:w-1/2 text-lg md:text-xl bottom-32 md:bottom-40 z-2">
+				<p class="font-lato absolute w-35/100 md:w-1/2 text-md md:text-lg md:text-xl bottom-32 md:bottom-40 z-2">
 					{page.desc}
 				</p>
 				<button class="font-grotesk bg-#6236FF/80 hover:bg-#6236FF hover:animate-swing hover:animate-1s px-3 py-2 pb-3 md:px-4 md:py-3 md:pb-4 absolute bottom-8 md:bottom-12 rounded-3xl text-3xl z-2">
