7281764c10b49e339b857d81a72c9023bc8d2ee4
seer geist <kusamaxi@gmail.com>
2022-04-25 18:42:06 -0500

    replace title linking

--- src/routes/welcome.svelte
+++ src/routes/welcome.svelte
@@ -15,6 +15,26 @@
 			p_6: 'Titles are designed to distinguish members of the program who have made active contributions to the Zeitgeist ecosystem and act as a source of prestige for those members. Titles also unlock various perks throughout the duration of the program including exclusive NFT rewards.',
 		}
 </script>
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>{page.title}</title>
+</svelte:head>
+
 <div class="rounded-2xl border-1 border-#6236FF min-h-100 p-10">
 	<h3 class="text-2xl font-bold">
 		{page.name}
